22fc9f1dd0c9a07d1f72c39ad5a8c5a52332a089
webbin <1358102877@qq.com>
2021-02-21 23:48:38 +0800

    fix bug, read temperature and insert to database

--- temperature/temperature_timer.py
+++ temperature/temperature_timer.py
@@ -3,7 +3,6 @@ import sched
 from datetime import datetime
 import os
 import sys
-from temperature import DHT22
 
 
 cwd = os.getcwd()
@@ -13,6 +12,7 @@ parent_path = '/'.join(splits)
 sys.path.append(parent_path)
 
 from sql.temperature_db import TemperatureDBUtil
+from temperature import DHT22
 
 
 def record_temperature_task(util: TemperatureDBUtil, schedule: sched.scheduler, duration):
@@ -23,13 +23,15 @@ def record_temperature_task(util: TemperatureDBUtil, schedule: sched.scheduler, 
     temperature = float(data['temperature'])
     timestamp = datetime.now()
     util.insert_temperature(timestamp, temperature, humidity)
-    schedule.enter(duration, 0, record_temperature_task)
+    schedule.enter(duration, 0, record_temperature_task, (util, schedule, duration))
 
 
 def main():
+    temp_database_util = TemperatureDBUtil('./temperature.db')
+    temp_database_util.start_connect()
     duration = 60
     schedule = sched.scheduler(time.time, time.sleep)
-    schedule.enter(0, 0, record_temperature_task, (schedule, duration))
+    schedule.enter(0, 0, record_temperature_task, (temp_database_util, schedule, duration))
     schedule.run()
 
 
